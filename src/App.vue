fa33374b50a15d4d53dd80e87dbc0a9be9528ba2
Weronika <104148980+Weroni-K@users.noreply.github.com>
2024-05-13 17:06:12 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,156 @@
+<script setup>
+import { ref } from 'vue';
+import IconWork from "./assets/icon-work.svg";
+import IconPlay from "./assets/icon-play.svg";
+import IconStudy from "./assets/icon-study.svg";
+import IconExercise from "./assets/icon-exercise.svg";
+import IconSocial from "./assets/icon-social.svg";
+import IconSelfCare from "./assets/icon-self-care.svg";
+import IconJeremy from "./assets/image-jeremy.png";
+
+let selectedOption = ref('daily');
+
+const activities = ref([
+  {
+    "title": "Work",
+    "iconUrl": IconWork,
+    "backgroundColor": "hsl(15, 100%, 70%)",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
+      }
+    }
+  },
+  {
+    "title": "Play",
+    "iconUrl": IconPlay,
+    "backgroundColor": "hsl(195, 74%, 62%)",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "iconUrl": IconStudy,
+    "backgroundColor": "hsl(348, 100%, 68%)",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "iconUrl": IconExercise,
+    "backgroundColor": "hsl(145, 58%, 55%)",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "iconUrl": IconSocial,
+    "backgroundColor": "hsl(264, 64%, 52%)",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23
+      }
+    }
+  },
+  {
+    "title": "Self Care",
+    "iconUrl": IconSelfCare,
+    "backgroundColor": "hsl(43, 84%, 65%)",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11
+      }
+    }
+  }
+]);
+</script>
+
+<template>
+  <div class="grid-container">
+    <div class="grid-item item1">
+      <div class="card-details">
+        <img class="avatar" :src="IconJeremy" alt="avatar">
+        <p>Report for</p><h2>Jeremy Robson</h2>
+      </div>
+      <div class="card-background">
+        <h3><span @click="selectedOption = 'daily'" :class="{ option: true, active: selectedOption === 'daily' }">Daily</span></h3>
+        <h3><span @click="selectedOption = 'weekly'" :class="{ option: true, active: selectedOption === 'weekly' }">Weekly</span></h3>
+        <h3><span @click="selectedOption = 'monthly'" :class="{ option: true, active: selectedOption === 'monthly' }">Monthly</span></h3>
+      </div>
+    </div>
+    <div class="grid-item" v-for="activity in activities" :key="activity.title" :style="{ backgroundImage: `url(${activity.iconUrl})`, backgroundColor: activity.backgroundColor }">
+      <div class="card-background"></div>
+      <div class="card-details">
+        <h3>{{ activity.title }}</h3>
+        <div v-if="selectedOption">
+          <h1>{{ activity.timeframes[selectedOption].current }}hrs</h1>
+          <p>Previous - {{ activity.timeframes[selectedOption].previous }}hrs</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
